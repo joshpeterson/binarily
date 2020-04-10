8122777b208343a3b9943f11faca32b2d83b365e
Josh Peterson <petersonjm1@gmail.com>
2020-04-10 19:56:23 +0000

    Add JSON support to communicate with JS code
    This changes adds the https://github.com/nlohmann/json library as
    a single header with a custom CMakeLists.txt file.
    
    It also changes the Emscripten interaction to return a JSON string.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,43 @@
-#if __EMSCPIPTEN__
+#if __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
 #endif
 
 #include "binary_buffer_reader.h"
 #include "type_detector.h"
+#include <json.hpp>
 
 using namespace binarily;
+using json = nlohmann::json;
 
 int main(int /*argc*/, char** /*argv*/)
 {
-#if __EMSCPIPTEN__
+#if __EMSCRIPTEN__
   EM_ASM("enableFileInput();");
 #endif
   return 0;
 }
 
-#if __EMSCPIPTEN__
+#if __EMSCRIPTEN__
 extern "C"
 {
   const char* EMSCRIPTEN_KEEPALIVE loadFileData(const uint8_t* buffer, int size)
   {
+    json data;
     BinaryBufferReader reader(buffer, size);
     TypeDetector detector(&reader);
     auto type = detector.Type();
     if (type == ELF32)
-      return "Elf 32-bit";
-    if (type == ELF64)
-      return "Elf 64-bit";
+      data["type"] = "Elf 32-bit";
+    else if (type == ELF64)
+      data["type"] = "Elf 64-bit";
+    else
+      data["type"] = "Unknown binary";
 
-    return "Unknown binary";
+    auto jsonString = data.dump();
+    auto jsonCopyForJs = (char*)malloc(jsonString.size() + 1);
+    strcpy(jsonCopyForJs, jsonString.c_str());
+
+    return jsonCopyForJs;
   }
 }
 #endif
